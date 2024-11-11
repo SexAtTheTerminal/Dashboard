e1ed8d2a74e0471be4cfc8c8ab8a836cb13207eb
DonPepino30 <gabrielcuba636@gmail.com>
2024-11-11 10:54:57 -0500

    LastTemplate

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
 
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('#theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode','active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode',null)
+}
+
+themeSwitch.addEventListener('click', () => {
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
